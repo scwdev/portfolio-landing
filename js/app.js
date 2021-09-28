e0d10dd2862f754e06132379d2323545f40eb0d6
Noam Blanks <noamblanks@gmail.com>
2021-09-27 22:29:56 -0400

    dev commit - boot-folio

--- js/app.js
+++ js/app.js
@@ -7,8 +7,29 @@ const client = contentful.createClient({
 });
 // This API call will request an entry with the specified ID from the space defined at the top, using a space-specific access token.
 
+const carousel = document.getElementById("carousel-inner")
+
 // console.log(client)
-client
-  .getEntries()
-  .then(response => console.log(response.items))
+client.getEntries()
+  .then(response => {
+    const projects = response.items
+    projects.forEach((item, index) => {
+      const fields = item.fields
+      carousel.insertAdjacentHTML('beforeend',
+        `<div id="card-${index}" class="carousel-item active">
+          <div class="card flex-row m-auto w-75" style="width: 18rem;">
+            <div class="card-body">
+              <h5 class="card-title">${fields?.title}</h5>
+              <p class="card-text">${fields?.description}</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+            <img src="${fields?.img.fields.file.url}" class="w-50 rounded" alt="..." />
+          </div>
+        </div>`
+      );
+
+      console.log(item.fields)
+    })
+    // console.log(response.items)
+  })
   .catch(err => console.log(err));
